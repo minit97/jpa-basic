2c8c730a3e90e0f46d2e673eadf96df961de4c5b
박현민 <dndb4902@naver.com>
2022-11-16 15:57:27 +0900

    # 영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,24 +15,35 @@ public class JpaMain {
         tx.begin();
 
         try {
+//            // 비영속
 //            Member member = new Member();
 //            member.setId(1L);
 //            member.setName("HelloA");
-//
+//            // 영속
 //            em.persist(member);
+//
+//            Member findMember1 = em.find(Member.class, 1L);
+//            Member findMember2 = em.find(Member.class, 1L);
+//            System.out.println(findMember2 == findMember1);
 
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("HelloJPA");
 
             // 대상이 테이블이 아닌 객체다!
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)  // 5번부터
-                    .setMaxResults(8)   // 8개 가져와라
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)  // 5번부터
+//                    .setMaxResults(8)   // 8개 가져와라
+//                    .getResultList();
+//
+//            for (Member member: result){
+//                System.out.println("member.name = " + member.getName());
+//            }
 
-            for (Member member: result){
-                System.out.println("member.name = " + member.getName());
-            }
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(150L, "A");
+
+            em.persist(member1);
+            em.persist(member2);
 
             tx.commit();
         } catch (Exception e) {

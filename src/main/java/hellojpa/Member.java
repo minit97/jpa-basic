959325cd27f84c7bc6d9abce9a71b218363c7795
박현민 <dndb4902@naver.com>
2022-11-22 14:34:42 +0900

    # 기본키 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,9 +6,21 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+//@SequenceGenerator(
+//        name="MEMBER_SEQ_GENERATOR",
+//        sequenceName = "MEMBER_SEQ",    // 매핑할 데이터베이스 시퀸스 이름
+//        initialValue = 1, allocationSize = 1)
+//@TableGenerator(
+//        name = "MEMBER_SEQ_GENERATOR",
+//        table = "MY_SEQUENCES",
+//        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
+//)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+//    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name")

858e9bf7567e531da16f123b0d025eca51421cf0
박현민 <dndb4902@naver.com>
2023-02-08 16:28:55 +0900

    #객체지향 쿼리 언어1 - 기본문법 : JPQL 함수

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -28,14 +28,8 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-//            String query =
-//                    "select " +
-//                            "case when m.age <= 10 then '학생요금' " +
-//                            "     when m.age >= 60 then '경로요금' " +
-//                            "     else '일반요금' " +
-//                            "end " +
-//                    "from Member m";
-            String query =  "select coalesce(m.username, '이름 없는 회원') from Member m ";
+            String query =  "select function('group_concat', m.username) from Member m ";
+//            String query =  "select group_concat(m.username) from Member m ";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
